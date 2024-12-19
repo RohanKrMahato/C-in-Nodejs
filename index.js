7723fc71693698b781fafed2d9176a178e5dd4ce
Rohan Kumar Mahato <rohankumarmahato345@gmail.com>
2024-12-20 01:37:19 +0530

    another aproach

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { metacall, metacall_load_from_file } = require('metacall');
+// const { metacall, metacall_load_from_file } = require('metacall');
 
-metacall_load_from_file('c', ['cfile.c']);
+// metacall_load_from_file('c', ['cfile.c']);
 
-metacall('getval',5);
+// metacall('getval',5);
+
+const {getval}=require('./cfile.c')
+
+console.log(getval(6));
